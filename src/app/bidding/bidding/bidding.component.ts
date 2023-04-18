6e678b0e1b36d360c92d9f1583536626c30867c7
sanghpal <kamblesanghpal2003@gmail.com>
2023-04-18 21:26:34 +0530

    asdfghjk

--- src/app/bidding/bidding/bidding.component.ts
+++ src/app/bidding/bidding/bidding.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AfterViewInit } from '@angular/core';
+
 @Component({
   selector: 'app-bidding',
   templateUrl: './bidding.component.html',
   styleUrls: ['./bidding.component.css']
 })
-export class BiddingComponent {
-  forms!:FormGroup
+export class BiddingComponent implements AfterViewInit {
+  
+  currentDate: any;
+  targetDate: any;
+  cDateMillisecs: any;
+  tDateMillisecs: any;
+  difference: any;
+  seconds: any;
+  minutes: any;
+  hours: any;
+  days: any;
+  year: number = 2023;
+  month: number = 6;
+  months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'April',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  day: number = 31;
+
+  ngAfterViewInit() {
+    this.myTimer();
+  }
+
+  myTimer() {
+     this.currentDate = new Date();
+     this.targetDate = new Date(2022, 6, 19);
+    this.cDateMillisecs = this.currentDate.getTime();
+    this.tDateMillisecs = this.targetDate.getTime();
+    this.difference = this.tDateMillisecs - this.cDateMillisecs;
+    this.seconds = Math.floor(this.difference / 1000);
+    this.minutes = Math.floor(this.seconds / 60);
+    this.hours = Math.floor(this.minutes / 60);
+    // this.days = Math.floor(this.hours / 24);
+
+    this.hours %= 24;
+    this.minutes %= 60;
+    this.seconds %= 60;
+    this.hours = this.hours < 10 ? this.hours : this.hours;
+    this.minutes = this.minutes < 10 ? this.minutes : this.minutes;
+    this.seconds = this.seconds < 10 ? this.seconds : this.seconds;
+
+    // document.getElementById('days').innerText = this.days;
+   var d = document.getElementById('hours') as HTMLElement
+   var m = document.getElementById('mins') as HTMLElement
+   var s = document.getElementById('seconds') as HTMLElement
+   d.innerText = this.hours
+   m.innerText = this.minutes
+   s.innerText = this.seconds
+   
+    setInterval(this.myTimer, 1000);
+  }
 }
