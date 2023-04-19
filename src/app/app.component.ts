ba484d6bcbc77f0b2d7fcbfb5678435d1864e6a2
sanghpal <kamblesanghpal2003@gmail.com>
2023-04-19 20:43:54 +0530

    asdfghjksdfghjk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,6 @@ import { timer } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sixtynine-sneakers';
-  timer:number = 86400
-  hours(){
-    
-  }
-  minutes(){
-
-  }
-  seconds(){
-
-  }
-  constructor() {
-this.tu()
-  }
-  tu(){
-    setTimeout(()=>{
-      console.log(this.timer--)
-      this.tu();
-     },1000)
-  }
+ 
 }
+
