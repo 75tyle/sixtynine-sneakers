4123e898ec8a2d4bf9867904198ba3d67ef0c9eb
sanghpal <kamblesanghpal2003@gmail.com>
2023-04-20 22:13:26 +0530

    sdfghjkjndkj

---
+++ src/app/Product.ts
@@ -0,0 +1,34 @@
+export const product = [
+    {
+        id: 1,
+        product_name: "Nike Air Max 90",
+        description: "Nike Air Max 90",
+        price: 69,
+        size: 'M',
+        image:'../../../assets/sneaker1.png'
+    },
+    {
+        id: 2,
+        product_name: "Jordan I High OG",
+        description: "Jordan I High OG",
+        price: 69,
+        size: 'M',
+        image:'../../../assets/sneaker2.jpg'
+    },
+    {
+        id: 3,
+        product_name: "Jordan 13 Retro",
+        description: "Jordan 13 Retro",
+        price: 69,
+        size: 'M',
+        image:'../../../assets/sneaker3.jpg'
+    },
+    {
+        id: 4,
+        product_name: "Addidas NMD R1",
+        description: "Addidas NMD R1",
+        price: 69,
+        size: 'M',
+        image:'../../../assets/sneaker4.jpg'
+    }
+]
--- src/app/main/sneaker-cards/sneaker-cards.component.ts
+++ src/app/main/sneaker-cards/sneaker-cards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { product } from 'src/app/Product';
 
 @Component({
   selector: 'app-sneaker-cards',
@@ -6,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sneaker-cards.component.css']
 })
 export class SneakerCardsComponent implements OnInit {
-  ngOnInit(): void {
+  
+  product:any[]=[]
+  snkr(id:any){
+    console.log(id)
+    for(let i = 0;i<product.length;i++){
+    console.log(product[i].id)
+    if(product[i].id == id){
+      console.log(product[i])
+      this.router.navigate(['bidding'])
+    }
+    
+    }
+    // this.router.navigate(['bidding'])
+  }
+
+  constructor(private router:Router) {
    
+    
+  }
+  
+  ngOnInit(): void {
+   console.log(product)
+   this.product = product
+
   }
 
 }
